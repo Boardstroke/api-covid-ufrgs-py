016c41d23ef7ec7235f239096ea0294468b6e8b7
Boardstroke <matheus.fachinis@gmail.com>
2020-04-19 22:30:24 -0300

    Primeiro Commit

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ setup(
   description = 'Uma interface para acessar dados do servidor relacionado a covid no RS',
   author = 'Matheus Fachini',                   # Type in your name
   author_email = 'matheus.fachinis@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url = 'https://github.com/Boardstroke/api-covid-ufrgs-py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Boardstroke/api-covid-ufrgs-py/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['API', 'COVID', 'RS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'requests',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
+    'Intended Audience :: Science/Research'
+    'Topic :: Scientific/Engineering',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
