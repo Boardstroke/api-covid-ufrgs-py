7c65958d75701bf29d92a2622232938f1fe95b51
Boardstroke <matheus.fachinis@gmail.com>
2020-04-19 22:35:56 -0300

    Primeiro Commit

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ setup(
           'requests',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Science/Research'
-    'Topic :: Scientific/Engineering',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
