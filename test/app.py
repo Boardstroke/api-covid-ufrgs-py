ddef56ea6f4c961414258c80d4a81ada02a85dec
Boardstroke <matheus.fachinis@gmail.com>
2020-04-19 22:21:52 -0300

    Primeiro Commit

--- test/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, '../api-covid-python')
-
-from matrix import Distancias
-
-d = Distancias()
-
-print(d.matriz_distancia())
